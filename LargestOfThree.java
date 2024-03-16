592fe543cecfeb43658631eaad0cb6d0078763ff
vinnytheprogrammer <58215501+vinnytheprogrammer@users.noreply.github.com>
2024-03-16 21:17:48 +0530

    Create LargestOfThree.java

---
+++ LargestOfThree.java
@@ -0,0 +1,22 @@
+public static Integer findLargest(Integer num1, Integer num2, Integer num3) {
+	//code here
+    if(num1 > num2){
+        
+        if(num1 > num3){
+            return num1;
+        }
+        else {
+            return num3;
+        }
+    }
+    else{
+
+        if(num2 > num3){
+            return num2;
+        }
+        else {
+            return num3;
+        }
+    }
+
+}
